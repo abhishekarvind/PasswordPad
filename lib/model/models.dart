25ac49ea8c6d57e872566840d5742cdc42400c0a
unknown <abhishekarvind07@gmail.com>
2022-07-16 19:20:30 +0530

    models

---
+++ lib/model/models.dart
@@ -0,0 +1,13 @@
+import 'package:hexcolor/hexcolor.dart';
+
+
+background(){
+  return HexColor('#28293D');
+}
+primary(){
+  return HexColor('#DEA5E8');
+}
+
+secondary(){
+  return HexColor('#BABABA');
+}
